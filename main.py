cbf4f0f398cfe34b8d4bc4537a4f76157e6f41c4
Chichigami <g.feng.work@gmail.com>
2024-08-20 17:45:50 -0400

    circleshape class added

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
 	pygame.init()
+	decouple = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	user = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill(color="black")
+		user.draw(screen)
+		user.update(dt)
 		pygame.display.flip()
 
+		dt = decouple.tick(60)/1000
 
 if __name__ == "__main__":
 	main()
